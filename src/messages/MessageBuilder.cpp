afebdb15c4a73b94c9446a5a20acdd9929e0ed7d
Peter <53868994+Pietro228@users.noreply.github.com>
2026-09-07 14:50:59 +0200

    Environments did not work for Twitch

--- src/messages/MessageBuilder.cpp
+++ src/messages/MessageBuilder.cpp
@@ -2769,7 +2769,7 @@ void MessageBuilder::appendStreamallyBadges(const QString &userID)
 
  */
 
-    auto saBadges = getApp()->getStreamAllyAPI()->getBadges(this->message_->platform, {userID}, "twitch");
+    auto saBadges = getApp()->getStreamAllyAPI()->getBadges(this->message_->platform, {userID}, this->message_->channelName);
 
     for (const auto badge : saBadges)
     {
